22b2eadee9016740eb746eea8c200ed227eafaec
Arto Mujunen <artomujunen@gmail.com>
2017-05-11 11:00:07 +0200

    Small change, testing build triggering in Jenkins

--- src/test/java/JenkinsTestTest.java
+++ src/test/java/JenkinsTestTest.java
@@ -11,7 +11,7 @@ public class JenkinsTestTest {
     @Test
     public void subtract() throws Exception {
         int result = JenkinsTest.subtract(10, 2);
-        Assert.assertEquals("### Wrong Subtract result: ", 5, result); 
+        Assert.assertEquals("### Wrong Subtract result: ", 5, result);
         logger.info("#### Subtract PASS");
     }
 
@@ -20,6 +20,7 @@ public class JenkinsTestTest {
         int result = JenkinsTest.multiply(10, 2);
         Assert.assertEquals("### Wrong Multiply result: ", 5, result);    // False failure
         logger.info("#### Multiply PASS");
+        
     }
 
 }
